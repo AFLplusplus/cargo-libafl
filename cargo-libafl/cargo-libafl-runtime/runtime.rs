70f38a36ca638594896ee74a561f04ece119f7d9
Michael Rodler <mrodler@amazon.de>
2023-05-05 09:35:58 +0200

    changed default log level to info for env_logger

--- cargo-libafl/cargo-libafl-runtime/runtime.rs
+++ cargo-libafl/cargo-libafl-runtime/runtime.rs
@@ -162,7 +162,7 @@ pub fn main() {
         rust_fuzzer_initialize();
     }
 
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let workdir = env::current_dir().unwrap();
 
